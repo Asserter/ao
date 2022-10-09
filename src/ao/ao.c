06cebba869677af27608c8a64d34085819c9de80
Ali Muhammed <uniapi@outlook.com>
2022-10-10 00:28:33 +0300

    Modified [src/ao/ao.c] - the <ge_case> generator can be run in place of <data>; 	the contract: <casesz> should be zero and <ge_case> should return the size of <aocase> when passed NULL

--- src/ao/ao.c
+++ src/ao/ao.c
@@ -5,12 +5,13 @@
  */
 
 #include <ao/ao.h>
+#include <alloca.h>
 
 static __thread ao_t _ao;
 
 static size_t ao_stats(const ao_t *ao)
 {
-	fstdout("Total Cases: %zu\n", ao->logger.nlog);
+	fstdout("\nTotal Cases: %zu\n", ao->logger.nlog);
 	fstdout("    Passed: %zu, Failed: %zu, Skipped: %zu\n",
 		ao->logger.passed, ao->logger.failed, ao->logger.skipped);
 	return ao->logger.nlog;
@@ -48,16 +49,23 @@ aop_t ao_log(aop_t aop, ao_t *ao)
 aop_t ao_assert(const char *descr, void *data, size_t ncase, size_t casesz,
 	aop_t (*assert_case)(void *aocase))
 {
+	void *aocase;
 	_ao.logger.nlog = _ao.logger.skipped = ncase;
 	_ao.logger.ndx = _ao.logger.passed = _ao.logger.failed = 0;
+	if (casesz)
+		aocase = (char *)data - casesz;
+	else if (!(aocase = alloca((*(size_t (*)(void *))data)(NULL))))
+		return AOP_SKIP;
 	fstdout("%s\n", descr);
 	while (ncase--) {
-		aop_t aop = (*assert_case)(data);
+		aop_t aop = (*assert_case)
+		(
+			casesz ? (aocase = (char *)aocase + casesz) : (*(void *(*)(void *))data)(aocase)
+		);
 		if (ao_log(aop, &_ao) == AOP_FAIL) {
 			ao_stats(&_ao);
 			return AOP_FAIL;
 		}
-		data = (char *)data + casesz;
 	}
 	ao_stats(&_ao);
 	return AOP_PASS;
@@ -68,27 +76,33 @@ static void ao_nop() { }
 aop_t ao_run(ao_t *ao)
 {
 	size_t ncase;
-	void *data;
+	void *aocase;
 	void (*at_case_fail)(const void *) = ao->at_case_fail ? ao->at_case_fail : (void (*)(const void *))ao_nop;
 	void (*at_case_exit)(void *) = ao->at_case_exit ? ao->at_case_exit : (void (*)(void *))ao_nop;
 	if (ao_setdefs(ao) != AOP_PASS)
 		return AOP_SKIP;
 	ncase = ao->ncase;
-	data = (char *)ao->data - ao->casesz;
+	if (ao->casesz)
+		aocase = (char *)ao->data - ao->casesz;
+	else if (!(aocase = alloca((*(size_t (*)(void *))ao->data)(NULL))))
+		return AOP_SKIP;
 	fstdout("%s\n", ao->descr);
 	while (ncase--) {
-		aop_t aop = (*ao->assert_case)(data = (char *)data + ao->casesz);
+		aop_t aop = (*ao->assert_case)
+		(
+			ao->casesz ? (aocase = (char *)aocase + ao->casesz) : (*(void *(*)(void *))ao->data)(aocase)
+		);
 		if (ao->inc == AOINC_AUTO)
 			ao_log(aop, ao);
 		if (aop == AOP_FAIL) {
-			(*at_case_fail)(data);
+			(*at_case_fail)(aocase);
 			if (ao->mode == AOMODE_STOP) {
-				(*at_case_exit)(data);
+				(*at_case_exit)(aocase);
 				ao_stats(ao);
+				return AOP_FAIL;
 			}
-			return AOP_FAIL;
 		}
-		(*at_case_exit)(data);
+		(*at_case_exit)(aocase);
 	}
 	ao_stats(ao);
 	return AOP_PASS;
